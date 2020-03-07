74c38d7295434363adb296634b646d2bf073349c
Jake Kile <jkile9@gmail.com>
2020-03-06 19:25:30 -0700

    began setting up api routing

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,3 +1,18 @@
+const fs = require("fs");
+
 module.exports = app => {
-    
+
+    app.get("/api/notes", function(req, res){
+        fs.readFile("db.json",(err, data) =>{
+            res.send(JSON.parse(data));
+        })
+    })
+
+    app.post("/api/notes", function(req, res){
+        fs.appendFile("db.json", JSON.stringify(req.body, null, 2), err => {
+            if(err) throw err;
+            res.send(req.body)
+        })
+    })
+
 }
